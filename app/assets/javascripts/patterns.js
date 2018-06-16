ccbb5d95f2a4aa01da398a5f44770982b8ce268a
Christina <github email address>
2018-06-16 13:16:27 -0600

    trying to fix 'debugger' and changed files to maybe help?

--- app/assets/javascripts/patterns.js
+++ app/assets/javascripts/patterns.js
@@ -1,11 +1,13 @@
 $(function() {
   $("form.button_to").on("submit", function(e) {
+    debugger
       //$.ajax({
+      //  url: this.action,
       //  method: "POST",
       //  url: this.action
       //}).success(function( response ) {
 //        $("div.favs").html(response)
-//        });
+      //  });
     e.preventDefault();
   });
 })
