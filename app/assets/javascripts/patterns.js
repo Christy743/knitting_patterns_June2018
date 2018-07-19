93bc2166829cb5a609e18df7ca9599abe9e8c4cf
Christina <github email address>
2018-07-19 15:18:42 -0600

    added the link pattern directions to click on and Pattern Directions is appended to the DOM.

--- app/assets/javascripts/patterns.js
+++ app/assets/javascripts/patterns.js
@@ -9,8 +9,8 @@ $(function() {
       dataType: 'json',
       data: $(this).serialize(),
     }).success(function( response ) {
-      //Put the list in alphabetical order using sort?
       $(".pattern_list").text("All Patterns")
+
         var patt_array = response.sort(function(pat1, pat2) {
           if (pat1["name"] < pat2["name"]) {
             return -1;
@@ -20,7 +20,7 @@ $(function() {
           }
             return 0;
         });
-        //debugger
+
         let patterns = patt_array.forEach(pattern => {
           let newPattern = new Pattern(pattern)
           let patternHtml = newPattern.formatIndex()
@@ -32,16 +32,14 @@ $(function() {
     function Pattern(pattern) {
       this.name = pattern.name
       this.id = pattern.id
-      this.content = pattern.content
-      this.comments = pattern.comments
-    }
+    };
 
     Pattern.prototype.formatIndex = function() {
       let patternHtml = `
         <a href="/patterns/${this.id}" data-id="${this.id}" class="show_pattern"><li>${this.name}</li></a>
       `
       return patternHtml
-    }
+    };
 })
 
  $(function() {
@@ -53,48 +51,26 @@ $(function() {
        url: this.action,
        dataType: 'json',
        data: $(this).serialize(),
-     }).success(function( response ){
+     }).success(function( response ) {
        $("#comment_content").val(null);
        $("div#comments ul").append(`<li>${response.content}</li>`);
        $("input[type=submit]").removeAttr('disabled');
      });
   });
-});
+})
 
 $(function() {
-  $(".pattern_list").on("click", function(e) {
+  $(".more_info").on("click", function(e) {
     e.preventDefault();
 
     $.ajax({
-      type: "GET",
+      type: this.method,
       url: this.href,
       dataType: 'json',
-    }).success(function( response ){
-      //debugger
-      //$(".more_info").hide();
-      //var id = $(this).response("id");
-      //$("div#more").append(response);
-      //$("input[type=click]").removeAttr('disabled');
-      let patterns = response.forEach(pattern => {
-        let newPattern = new Pattern(pattern)
-        let patternHtml = newPattern.formatIndex()
-        $('#more').append(patternHtml)
-      });
-    });
-  });
-
-  function Pattern(pattern) {
-    this.name = pattern.name
-    this.id = pattern.id
-    this.content = pattern.content
-    this.comments = pattern.comments
-  }
-
-  Pattern.prototype.formatIndex = function() {
-    let patternHtml = `
-      <a href="/patterns/${this.id}" data-id="${this.id}" class="show_pattern"><li>More...</li></a>   
-    `
-                                                                            //<li>${this.content}</li>
-    return patternHtml
-  }
+      data: $(this).serialize(),
+    }).success(function( response ) {
+      $(".more_info").hide();
+      $("#commented").append(`${response.content}`);
+    })
+  })
 })
